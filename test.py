32c922fe5a60543f1609188ac76184cfeeb1e798
xuzhiyong <xuzhiyong@hotmail.com>
2020-06-14 23:00:17 +0800

    项目首次提交

---
+++ test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+# @Time    : $[YEAR]-$[MONTH]-$[DAY] $[HOUR]:$[MINUTE]:$[SECOND]
+# @Author  : xuzhiyong
+# @Site    : 
+# @File    : test.py
+# @Software: PyCharm
+
+print('你好，学习Python!')
+print('Hello')
+
